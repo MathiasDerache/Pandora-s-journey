26e1d36214fe246f52280175bbc07b7739bed54f
Elodie <ruelelodie@gmail.com>
2020-10-26 15:44:31 +0100

    MAJ include

--- Forum/Pandora_Forum_accueil/forum.php
+++ Forum/Pandora_Forum_accueil/forum.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="style.css"> 
-    <title>Footer</title>
+    <title>Forum</title>
 </head>
 
 <body class="position-relative">
