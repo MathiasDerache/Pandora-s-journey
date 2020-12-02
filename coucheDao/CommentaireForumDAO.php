5b04e1635f969a42d2ab22ff9a3eead6a2e9902a
Mathias <mat.derache@gmail.com>
2020-12-02 16:38:30 +0100

    Ajout dao sujet theme et commentaire forum

---
+++ coucheDao/CommentaireForumDAO.php
@@ -0,0 +1,93 @@
+<?php
+
+include_once("interfaceDao.php");
+include_once("connectionBaseDonnees.php");
+include_once("../classe_metier/CommentaireSujet.php");
+
+    class CommentaireForumDAO implements InterfDao{
+
+
+        public function create(CommentaireSujet $commSujet){
+
+            try{
+            $db = $this->db->connectiondb();
+            $idCommSuj = $commSujet->getIdCommSuj();
+            $pseudoUt = $commSujet->getPseudoUt();              // Function create Commentaire
+            $dateCommSuj = $commSujet->getDateCommSuj()->format('Y-m-d');
+            $contCommSuj = $commSujet->getContCommSuj();
+            $idSuje = $commSujet->getIdSuje();
+            $idUti = $commSujet->getIdUti();
+            $stm = $db->prepare("INSERT INTO commentairefurum VALUES(?,?,?,?,?,?)");
+            $stm->bindValue(1, $idCommSuj, PDO::PARAM_INT);
+            $stm->bindValue(2, $pseudoUt);
+            $stm->bindValue(3, $dateCommSuj);
+            $stm->bindValue(4, $contCommSuj);
+            $stm->bindValue(5, $idSuje, PDO::PARAM_INT);
+            $stm->bindValue(6, $idUti,  PDO::PARAM_INT);
+            $stm->execute();
+            } catch (PDOException $f) {
+                throw new DaoException($f->getCode(), $f->getMessage());
+            }
+        }  
+
+        public function read(): array
+        {
+            try {
+                $db = $this->db->connectiondb();
+                $stm = $db->prepare("SELECT * FROM commentairefurum");
+                $stm->execute();
+                $array = $stm->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $f) { 
+                throw new DaoException($f->getCode(), $f->getMessage());            // Function Read Commentaires
+            }
+            $tab = [];
+            foreach ($array as $value) {
+                $commSujet = new CommentaireSujet();
+                $commSujet->setIdCommSuj($value['idcommSujet'])->setPseudoUt($value['psodoUt'])
+                    ->setDateCommSuj(new DateTime($value['dateComSuj']))
+                    ->setContCommSuj($value['contComSuj'])->setIdSuje($value['idSujetTh'])
+                    ->setIdUti($value['idUti']);
+    
+                $tab[] = $commSujet;
+            }
+            return $tab;
+        }
+
+
+        public function update(CommentaireSujet $commSujet){
+
+            try{
+            $db = $this->db->connectiondb();
+            $idCommSuj = $commSujet->getIdCommSuj();
+            $pseudoUt = $commSujet->getPseudoUt();                                      // Function update Commentaire
+            $dateCommSuj = $commSujet->getDateCommSuj();
+            $contCommSuj = $commSujet->getContCommSuj();
+            $idSuje = $commSujet->getIdSuje();
+            $idUti = $commSujet->getIdUti();
+            $stm = $db->prepare("UPDATE commentairefurum SET psodoUt=?, dateComSuj=?, contComSuj=?, idSujetTh=?, idUti=?  WHERE idcommSujet =?");
+            $stm->bindValue(1, $pseudoUt);
+            $stm->bindValue(2, $dateCommSuj);
+            $stm->bindValue(3, $contCommSuj);
+            $stm->bindValue(4, $idSuje,  PDO::PARAM_INT);
+            $stm->bindValue(5, $idUti,  PDO::PARAM_INT);
+            $stm->bindValue(6, $idCommSuj,  PDO::PARAM_INT);
+            $stm->execute();
+            } catch (PDOException $f) {
+                throw new DaoException($f->getCode(), $f->getMessage());
+            }
+        }
+
+        
+        public function delete(int $id){
+
+            try{
+            $db = $this->db->connectiondb();            
+            $stm = $db->prepare("DELETE FROM commentairefurum WHERE idcommSujet = ?");     // Function Delete Commentaire
+            $stm->bindValue(1, $id, PDO::PARAM_INT);
+            $stm->execute();
+            } catch(PDOException $f){
+                throw new DaoException($f->getCode(), $f->getMessage());
+            }
+        }  
+    }
+?>
---
+++ coucheDao/SujetForumDAO.php
@@ -0,0 +1,83 @@
+<?php
+
+include_once("interfaceDao.php");
+include_once("connectionBaseDonnees.php");
+include_once("../classe_metier/SujetTheme.php");
+
+    class SujetForumDAO implements InterfDao{
+
+
+        public function create(SujetTheme $sujetTheme){
+
+            try{
+            $db = $this->db->connectiondb();
+            $idSuje = $sujetTheme->getIdSujetForum();
+            $intSujet = $sujetTheme->getIntSujet();   
+            $idTheme = $sujetTheme->getIdTheme();           // Function create Commentaire
+            $idUti = $sujetTheme->getIdUti();
+            $stm = $db->prepare("INSERT INTO sujetfurum VALUES(?,?,?,?)");
+            $stm->bindValue(1, $idSuje,  PDO::PARAM_INT);
+            $stm->bindValue(2, $intSujet);
+            $stm->bindValue(3, $idTheme,  PDO::PARAM_INT);
+            $stm->bindValue(4, $idUti,  PDO::PARAM_INT);
+            $stm->execute();
+            } catch (PDOException $f) {
+                throw new DaoException($f->getCode(), $f->getMessage());
+            }
+        }  
+
+        public function read(): array
+        {
+            try {
+                $db = $this->db->connectiondb();
+                $stm = $db->prepare("SELECT * FROM sujetfurum");
+                $stm->execute();
+                $array = $stm->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $f) { 
+                throw new DaoException($f->getCode(), $f->getMessage());            // Function Read Commentaires
+            }
+            $tab = [];
+            foreach ($array as $value) {
+                $sujetTheme = new SujetTheme();
+                $sujetTheme->setIdSujetForum($value['idSujetTh'])->setIntSujet($value['typeSujetTh'])
+                    ->setIdTheme($value['idTheme'])
+                    ->setIdUti($value['idUti']);
+    
+                $tab[] = $sujetTheme;
+            }
+            return $tab;
+        }
+
+        public function update(SujetTheme $sujetTheme){
+
+            try{
+            $db = $this->db->connectiondb();
+            $idSuje = $sujetTheme->getIdSujetForum();
+            $intSujet = $sujetTheme->getIntSujet();   
+            $idTheme = $sujetTheme->getIdTheme();           // Function update Commentaire
+            $idUti = $sujetTheme->getIdUti();
+            $stm = $db->prepare("UPDATE sujetfurum SET typeSujetTh=?, idTheme=?, idUti=? WHERE idSujetTh =?");
+            $stm->bindValue(1, $intSujet);
+            $stm->bindValue(2, $idTheme,  PDO::PARAM_INT);
+            $stm->bindValue(3, $idUti,  PDO::PARAM_INT);
+            $stm->bindValue(4, $idSuje,  PDO::PARAM_INT);
+            $stm->execute();
+            } catch (PDOException $f) {
+                throw new DaoException($f->getCode(), $f->getMessage());
+            }
+        }
+
+        
+        public function delete(int $id){
+
+            try{
+            $db = $this->db->connectiondb();               
+            $stm = $db->prepare("DELETE FROM sujetfurum WHERE idSujetTh = ?");     // Function Delete Commentaire
+            $stm->bindValue(1, $id, PDO::PARAM_INT);
+            $stm->execute();
+            } catch (PDOException $f) {
+                throw new DaoException($f->getCode(), $f->getMessage());
+            };
+        }  
+    }
+?>
