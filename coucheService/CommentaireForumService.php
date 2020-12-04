318e70ae6aeae07137f23fcb193b80716198e8b5
Mathias <mat.derache@gmail.com>
2020-12-04 11:22:39 +0100

    Ajout service

---
+++ coucheService/CommentaireForumService.php
@@ -0,0 +1,32 @@
+<?php
+include_once("../coucheDao/CommentaireForumDao.php");
+include_once("InterfService.php");
+
+
+    class CommentaireForumService implements interfService{
+
+        public function __construct()
+        {
+            $this->service = new CommentaireAnnDao();
+        }
+
+        public function creatService(object $object): object
+        {
+            return $this->service->creat($object);              // Function create Commentaire
+        }
+
+        public function readService(): array
+        {   
+            return $this->service->read();              // Function Read Commentaires
+        }
+
+        public function updateService(object $object): object
+        {
+            return $this->service->update($object);             // Function update Commentaire
+        }
+
+        public function deleteService(int $id): int
+        {
+            return $this->service->delete($id);         // Function Delete Commentaire
+        }
+    }
