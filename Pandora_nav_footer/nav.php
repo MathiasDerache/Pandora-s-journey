30d6c62d5728e297ea97723b62693b217d70fbd0
Elodie <ruelelodie@gmail.com>
2021-01-02 19:26:30 +0100

    finalisation de l'affichage de la liste des annonces en un seul controlleur et MAJ des liens dans la navbar et la page accueil

--- Pandora_nav_footer/nav.php
+++ Pandora_nav_footer/nav.php
@@ -9,9 +9,9 @@ function navBar()
             </div>
             <div class="col-sm-12 col-md-12 col-lg-9 col-xl-9 listnav">
                 <ul>
-                    <li><a href="../pages_anonces/immobilier.php" class="nav-link text-white">Immobilier</a></li>
-                    <li><a href="../pages_anonces/travail.php" class="nav-link text-white">Travail</a></li>
-                    <li><a href="../pages_anonces/loisirs.php" class="nav-link text-white">Loisir</a></li>
+                    <li><a href="../coucheControlleur/annoncescontroleur.php?action=annonces_immobilier" class="nav-link text-white">Immobilier</a></li>
+                    <li><a href="../coucheControlleur/annoncescontroleur.php?action=annonces_travail" action="annonces_travail" class="nav-link text-white">Travail</a></li>
+                    <li><a href="../coucheControlleur/annoncescontroleur.php?action=annonces_loisir" action="annonces_loisir" class="nav-link text-white">Loisir</a></li>
                     <li><a href="../page_contact/contact.php" class="nav-link text-white">Contacts</a></li>
                 </ul>
             </div>
--- coucheControlleur/annoncescontroleur.php
+++ coucheControlleur/annoncescontroleur.php
@@ -1,21 +1,37 @@
 <?php
 
-require_once("../pages_anonces/immobilier.php");
-require_once("../pages_anonces/loisirs.php");
-require_once("../pages_anonces/travail.php");
+require_once("../pages_anonces/head_annonce.php");
+require_once("../pages_anonces/body_annonce.php");
+require_once("../pages_anonces/card_annonce.php");
+require_once("../pages_anonces/pagination_annonce.php");
 require_once("../coucheService/AnnoncesService.php");
 require_once("../Pandora_nav_footer/nav.php");
 require_once("../Pandora_nav_footer/footer.php");
 
+if (!empty($_GET)){
+    if (isset($_GET['action']) && $_GET['action'] == "annonces_immobilier"){
+        $title = "Immobilier";
+        headAnnonce($title);
+    }
+    elseif(isset($_GET['action']) && $_GET['action'] == "annonces_travail"){
+        $title = "Travail";
+        headAnnonce($title);
+    }
+    elseif(isset($_GET['action']) && $_GET['action'] == "annonces_loisir"){
+        $title = "Loisir";
+        headAnnonce($title);
+    }
+}
+
 navBar();
-body();
+bodyAnnonces();
 
 if (!empty($_GET)){
     if (isset($_GET['action']) && $_GET['action'] == "annonces_immobilier"){
         $annonces = (new AnnoncesService())->readService();
         for ($i=0; $i<count($annonces); $i++){
             if ($annonces[$i]->getTypeAnn() == 'immobilier'){
-                card($annonces, $i);
+                cardAnnonce($annonces, $i);
             }
         }
     }
@@ -23,7 +39,7 @@ if (!empty($_GET)){
         $annonces = (new AnnoncesService())->readService();
         for ($i=0; $i<count($annonces); $i++){
             if ($annonces[$i]->getTypeAnn() == 'travail'){
-                card($annonces, $i);
+                cardAnnonce($annonces, $i);
             }
         }
     }
@@ -31,11 +47,11 @@ if (!empty($_GET)){
         $annonces = (new AnnoncesService())->readService();
         for ($i=0; $i<count($annonces); $i++){
             if ($annonces[$i]->getTypeAnn() == 'loisir'){
-                card($annonces, $i);
+                cardAnnonce($annonces, $i);
             }
         }
     }
 }
 
-pagination();
+paginationAnnonce();
 footer();
