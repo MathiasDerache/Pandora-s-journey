c33adf272adcf87244d757d1d06bf3fddc2a30c6
florent <mertens.florent00@gmail.com>
2021-01-04 18:22:53 +0100

    Modification des methodes F

--- coucheService/UtilisateurService.php
+++ coucheService/UtilisateurService.php
@@ -39,9 +39,4 @@ class UtilisateurService implements interfService
     {
         return $this->service->delete($id);
     }
-
-    public function trouveUtil(?int $util)
-    {
-        return $this->service->trouveUtilisateur($util);
-    }
 }
