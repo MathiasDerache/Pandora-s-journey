a2fff440fdf74ef35896244f61202c9bb6fd425f
florent <mertens.florent00@gmail.com>
2021-01-12 17:22:50 +0100

    Ajout methode searchByEmail F

--- coucheDao/UtilisateurDao.php
+++ coucheDao/UtilisateurDao.php
@@ -23,7 +23,7 @@ class UtilisateurDao implements InterfDao
             $numTel = $object->getNumTel();
             $password = $object->getPassword();
             $profil = $object->getProfil();
-            $dateNaissance = $object->getDateNaissance();
+            $dateNaissance = $object->getDateNaissance()->format("Y-m-d");
             $civilite = $object->getCivilite();
             $stm->bindValue(1, $nom);
             $stm->bindValue(2, $prenom);
@@ -128,5 +128,31 @@ class UtilisateurDao implements InterfDao
                 ->setDateNaissance(new DateTime($value['dateNaissance']))->setCivilite($value['sex']);
         }
         return $utilisateur;
+    }
+
+    public function searchByEmail(string $email): ?object
+    {
+        try {
+            $db = $this->db->connectiondb();
+            $stm = $db->prepare("SELECT * FROM utilisateur WHERE email=?");
+            $stm->bindValue(1, $email);
+            $stm->execute();
+            $array = $stm->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $f) {
+            throw new DaoException($f->getCode(), $f->getMessage());
+        }
+        if (!empty($array)) {
+            foreach ($array as $value) {
+                $utilisateur = new Utilisateur();
+                $utilisateur->setIdUti($value['idUti'])->setNom($value['nom'])->setPrenom($value['prenom'])
+                    ->setPseudo($value['pseudo'])->setEmail($value['email'])
+                    ->setNumTel($value['numeroTel'])->setPassword($value['passWord'])->setProfil($value['profil'])
+                    ->setDateNaissance(new DateTime($value['dateNaissance']))->setCivilite($value['sex']);
+            }
+            return $utilisateur;
+        } else {
+            $utilisateur = null;
+            return $utilisateur;
+        }
     }
 }
