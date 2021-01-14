3692bf0372e3871b4e746347e84b5e39b653ae5f
rugaba_rene <rugabarj@gmail.com>
2021-01-14 16:12:38 +0100

    modif link

--- Forum/forum_sujet/forum_sujet_com.php
+++ Forum/forum_sujet/forum_sujet_com.php
@@ -89,14 +89,14 @@ function SujetThemeForum(SujetTheme $sujetTheme, ?CommentaireSujet $comUpdate)
                                                 <p class="mb-0">Posté le
                                                     <?php echo $value->getDateCommSuj()->format('d-m-Y'); ?>
                                                     <span class="modifElem float-right  btn btn-warning rounded-pill shadow" data-toggle="modal" data-target="#modalForumAnnonces">
-                                                        <a href="forumcontroleur.php?idSujForum=<?php echo $value->getIdSuje(); ?>&idCommForum=<?php echo $value->getIdCommSuj(); ?>&action=recupe_pour_modif&#modalForumAnnonces" class="modifierSpan">
+                                                        <a href="forumcontrolleur.php?idSujForum=<?php echo $value->getIdSuje(); ?>&idCommForum=<?php echo $value->getIdCommSuj(); ?>&action=recupe_pour_modif&#modalForumAnnonces" class="modifierSpan">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
                                                                 <path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
                                                             </svg>
                                                         </a>
                                                     </span>
                                                     <span class="float-right  btn btn-danger rounded-pill shadow mr-2">
-                                                        <a href="forumcontroleur.php?idCommForum=<?php echo $value->getIdCommSuj(); ?>&idSujForum=<?php echo $value->getIdSuje(); ?>&action=delete" class="text-white">
+                                                        <a href="forumcontrolleur.php?idCommForum=<?php echo $value->getIdCommSuj(); ?>&idSujForum=<?php echo $value->getIdSuje(); ?>&action=delete" class="text-white">
                                                             <svg xmlns="http://www.w3.org/2000/svg" height="2rem" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                                                             </svg>
