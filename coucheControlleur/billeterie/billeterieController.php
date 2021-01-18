f5a369590bd2e646b35ccfed5abe5fd51ea89a43
Mathias <mat.derache@gmail.com>
2021-01-18 16:24:13 +0100

    Billeterie controleur

---
+++ coucheControlleur/billeterie/billeterieController.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once("../../Pandora_nav_footer/nav.php");
+include_once("../../Pandora_nav_footer/footer.php");
+include_once("../../view/billeterie/index.php");
+
+head();
+navBar();
+etapes();
+complementaire();
+formulaireVol();
+select();
+suiteFormulaireVol();
+footer();
+?>
