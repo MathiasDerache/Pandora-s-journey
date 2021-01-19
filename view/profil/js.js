a259b50e84a421c73004daa809c0cc8c8d55e58a
florent <mertens.florent00@gmail.com>
2021-01-19 16:42:25 +0100

    modif view profil F

--- view/profil/js.js
+++ view/profil/js.js
@@ -1,28 +1,63 @@
 // CountDown
-// Date de départ
-var countDownDate = new Date("2021-01-22").getTime();
 
-// Actualise le compte à rebours toutes les secondes
-var x = setInterval(function() {
+var httpRequest = new XMLHttpRequest();
+httpRequest.open("GET", "profilControleur.php?action=countDown", true)
+httpRequest.send()
 
-    // Date d'aujourd'hui en millisecondes
-    var now = new Date().getTime();
-
-    // Différence entre la date de départ et la date d'aujourd'hui
-    var distance = countDownDate - now;
-
-    // Calcul du temps pour les jours, les heures, les minutes et les secondes
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Affiche le résultat dans un élément avec l'id="countdown"
-    document.getElementById("countDays").innerHTML = days;
-    document.getElementById("countHours").innerHTML = hours;
-    document.getElementById("countMinutes").innerHTML = minutes;
-    document.getElementById("countSeconds").innerHTML = seconds;
-}, 1000);
+httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+        var dateEmb = httpRequest.responseText
+        console.log(dateEmb);
+        if (dateEmb !== "") {   
+            // Date de départ
+            var countDownDate = new Date(dateEmb).getTime();
+    
+            // Actualise le compte à rebours toutes les secondes
+            var x = setInterval(function() {
+                // Date d'aujourd'hui en millisecondes
+                var now = new Date().getTime();
+    
+                // Différence entre la date de départ et la date d'aujourd'hui
+                var distance = countDownDate - now;
+    
+                // Calcul du temps pour les jours, les heures, les minutes et les secondes
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+                // Affiche le résultat dans un élément avec l'id="countdown"
+                document.getElementById("countDays").innerHTML = days;
+                document.getElementById("countHours").innerHTML = hours;
+                document.getElementById("countMinutes").innerHTML = minutes;
+                document.getElementById("countSeconds").innerHTML = seconds;
+                
+            }, 1000);
+        } else {
+            var countDown = document.getElementById("countDown");
+            countDown.removeAttribute('class');
+            countDown.innerHTML = "";
+            countDown.innerHTML = "Vous n'avez pas encore de voyage de prévue";
+            countDown.style.fontSize = "x-large";
+            var div1 = document.createElement("div");
+            div1.className = "container";
+            var div2 = document.createElement("div");
+            div2.className = "text-center pt-2";
+            var boutton = document.createElement("boutton");
+            boutton.className = "btn btn-warning rounded-pill text-white";
+            var a = document.createElement("a");
+            a.href = "../billeterie/billeterieController.php";
+            var t = document.createTextNode("Billeterie");
+            boutton.style.fontSize = "large";
+            boutton.style.textShadow = "black 1px 1px 1px"
+            boutton.appendChild(t);
+            boutton.appendChild(a);
+            div2.appendChild(boutton);
+            div1.appendChild(div2);
+            countDown.appendChild(div1);
+        }
+    }
+}
 
 // Menu
 
--- view/profil/profilUtilisateur.php
+++ view/profil/profilUtilisateur.php
@@ -178,46 +178,13 @@ function vosSujets($arraySujet, $arrayReponse)
     </div>
 <?php }
 
-function locationAchat()
-{ ?>
-    <div class="col-sm-6 col-md-6 col-lg-6 mt-5">
-        <h2 class="text-center text-white">LOCATIONS / ACHATS</h2>
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="../../view/profil/images/voyagez.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="../../view/profil/images/voyagez.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="../../view/profil/images/voyagez.jpg" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
-<?php }
-
 function compteARebours()
 { ?>
     <div class="col-sm-12 col-md-12 col-lg-12 mt-5">
         <div class="row">
             <div class="col-12 text-center text-white">
                 <h2>Votre voyage commence dans</h2>
-                <div class="row countDown">
+                <div class="row countDown" id="countDown">
                     <div>
                         <div id="countDays">
                         </div>
